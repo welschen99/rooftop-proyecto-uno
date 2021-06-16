01807d89d481a7bb7a2d726d1e6c14670615fb6d
welschen99 <welschen99@gmail.com>
2021-06-16 08:25:51 -0300

    data hasta clase 3

--- app.js
+++ app.js
@@ -2,28 +2,44 @@ const express = require('express')//importamos express
 const app = express()//creamos la app
 const path=require('path')//se encarga de usar el caracter adecuado segun el sistema
 const bodyParser = require('body-parser')//funcion libreria toma la peticion antes y desp convierte a un objeto js
+const products = require('./data')
+
 //http://localhost:3000/
 
 //middlewares
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-app.get('/',function(req,res){//get peticion dominio mas una '/'
-    let file = path.resolve('src','index.html')//envia la direccion completa sin problemas
-    res.sendFile(file)//envia como respuesta el file
+app.get('/', function(req,res){
+    res.json()
 })
 
-app.post('/', function(req,res){
-    res.send(req.body)
+app.get('/products', function(req,res){
+    console.log(req.query.price)
+    //http://localhost:3000/products?page=5 lo convierte en js y se muestra en la consola
+    res.json(products)
+})
+//http://localhost:3000/products para verlo
+
+app.get('/products/:id', function(req,res){
+    console.log(req.params.id)//para acceder a ese parametro designado en la url
+    res.json()
 })
 
-// app.get('/viejo', function(req,res){
-// res.send('nuevo ')
-// })
+app.post('/products', function(req,res){
+    console.log(req.body)
+    if(true){
+        res.status(403).json({message:'Cannot post'})//ves es postman que funcniona
+    }
+    res.json()
+})
 
-// app.get('/nuevo', function(req,res){
-// res.send('Esta es la nueva ubicacion')
-// })
+app.patch('/products/:id', function(req,rest){
+    res.json()
+})
+
+app.delete('/products/:id', function(req,res){
+    res.json()
+})
 
 app.listen(3000)//en que puerto esta
---
+++ data.js
@@ -0,0 +1,18 @@
+let productos =[
+    {
+        "id":1,
+        "title":"...",
+        "descripcion":"..."
+    },
+    {
+        "id":2,
+        "title":"...2",
+        "descripcion":"...2"
+    },
+    {
+        "id":3,
+        "title":"...3",
+        "descripcion":"...3"
+    }
+]
+module.exports=productos
